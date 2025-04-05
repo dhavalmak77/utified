80a57d2089db08c8dd415ba29e6072c25ac66a72
dhavalmak77 <dhavalmak77@gmail.com>
2025-04-06 00:46:26 +0530

    Add mantine UI system

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/@mantine/core/**/*.{ts,tsx}',
+		'./node_modules/@mantine/hooks/**/*.{ts,tsx}',
+		'./node_modules/@mantine/form/**/*.{ts,tsx}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+}
